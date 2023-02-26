bd551ae5107ab5c75207ea0535e935b32ee66217
ker0lajn <karolinatresk@gmail.com>
2023-02-26 23:14:57 +0100

    Added icon and unit conversion

--- script.js
+++ script.js
@@ -31,43 +31,70 @@ form.addEventListener("submit", function (event) {
   searchCity(input.value);
 });
 
-function unitCelsius(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let celsius = document.querySelector("#temperature");
-  celsius.innerHTML = `11`;
+  let temperatureElement = document.querySelector("#temperature");
+  if (celsiusTemperature === null) {
+    celsiusTemperature = Math.round(
+      ((temperatureElement.dataset.temperature - 32) * 5) / 9
+    );
+  }
+  temperatureElement.innerHTML = `${celsiusTemperature}`;
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.classList.add("active");
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.classList.remove("active");
 }
 
-//let tempCelsius = document.querySelector("#celsius-link");
-//tempCelsius.addEventListener("click", unitCelsius);
-
-function unitFahrenheit(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let fahrenheit = document.querySelector("#temperature");
-  fahrenheit.innerHTML = `54`;
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = Math.round(
+    (temperatureElement.dataset.temperature * 9) / 5 + 32
+  );
+  temperatureElement.innerHTML = `${fahrenheitTemperature}`;
+  celsiusTemperature = null;
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.classList.remove("active");
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.classList.add("active");
 }
 
-let tempFahrenheit = document.querySelector("#fahrenheit-link");
-tempFahrenheit.addEventListener("click", unitFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+
 
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = temperature;
+  currentTemp.dataset.temperature = temperature;
+    celsiusTemperature = null;
   let city = document.querySelector("h1");
   city.innerHTML = response.data.name;
-  currentTemp.innerHTML = temperature;
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = Math.round(response.data.wind.speed * 3.6);
-  let descriptionWeather = document.querySelector ("#description");
-  descriptionWeather.innerHTML = response.data.weather[0].description;
-  console.log(response.data.weather[0].description);
-
-  
+  let descriptionWeather = document.querySelector("#description");
+  let description = response.data.weather[0].description;
+  descriptionWeather.innerHTML =
+    description.charAt(0).toUpperCase() + description.slice(1);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
 }
 
 function searchCity(city) {
   let apiKey = "4ea1510a05549d524d10fc1d91f0154d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
-               ${city}&units=metric&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
 }
 
